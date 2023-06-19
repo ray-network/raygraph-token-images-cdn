54baa7a74c42fc725080fad19ac5e7cd659d98a0
ray-robot <hello@raynetwork.io>
2023-06-19 12:08:36 +0300

    Fix networks .env variable

--- src/services/koios-tiny-client.ts
+++ src/services/koios-tiny-client.ts
@@ -1,5 +1,9 @@
 import KoiosTinyClient from "koios-tiny-client"
+import dotenv from "dotenv"
 
-const { client, methods: Koios } = new KoiosTinyClient('https://mainnet.blockchain.raygraph.io')
+dotenv.config()
+const KOIOS_API = process.env.KOIOS_API
+
+const { client, methods: Koios } = new KoiosTinyClient(KOIOS_API)
 
 export default Koios
